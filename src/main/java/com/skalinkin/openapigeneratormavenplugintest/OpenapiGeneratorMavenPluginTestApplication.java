3f4a6a3e2a48c8c22055c44852963a776ed112eb
Sergiy Kalinkin <Sergiy.Kalinkin@motorolasolutions.com>
2020-12-25 10:20:53 -0500

    add openapi maven plugin

---
+++ src/main/java/com/skalinkin/openapigeneratormavenplugintest/OpenapiGeneratorMavenPluginTestApplication.java
@@ -0,0 +1,13 @@
+package com.skalinkin.openapigeneratormavenplugintest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenapiGeneratorMavenPluginTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenapiGeneratorMavenPluginTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skalinkin/openapigeneratormavenplugintest/OpenapiGeneratorMavenPluginTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skalinkin.openapigeneratormavenplugintest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenapiGeneratorMavenPluginTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
